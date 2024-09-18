5b8847d94bac475e9b3bc7d00c75048fb63ea9ff
AndersonXth <117678580+AndersonXth@users.noreply.github.com>
2024-09-18 12:18:49 -0300

    adicionado a funcionalidade de envio pelo wpp

--- js/app.js
+++ js/app.js
@@ -12,6 +12,8 @@ var VALOR_CARRINHO = 0
 
 var VALOR_ENTREGA = 5
 
+var CELULAR_EMPRESA = '5585986586018'
+
 cardapio.eventos = {
     init: () => {
         cardapio.metodos.obterItensCardapio();
@@ -368,6 +370,34 @@ cardapio.metodos = {
 
         $("#resumoEndereco").html(`${MEU_ENDERECO.endereco}, ${MEU_ENDERECO.numero}, ${MEU_ENDERECO.complemento}, ${MEU_ENDERECO.bairro}`);
         $("#cidadeEndereco").html(`${MEU_ENDERECO.cidade}-${MEU_ENDERECO.uf} / ${MEU_ENDERECO.cep}`);
+
+        cardapio.metodos.finalizarPedido();
+    },
+
+    finalizarPedido: () => {
+        if(MEU_CARRINHO.length > 0 && MEU_CARRINHO != null){
+            var texto = 'Olá, gostaria de fazer um pedido:';
+            texto += `\n*Itens do pedido:*\n\n\${itens}`;
+            texto += '\n*Endereço de entrega:*';
+            texto += `\n${MEU_ENDERECO.endereco}, ${MEU_ENDERECO.numero}, ${MEU_ENDERECO.complemento}, ${MEU_ENDERECO.bairro}`;
+            texto += `\n${MEU_ENDERECO.cidade}-${MEU_ENDERECO.uf} / ${MEU_ENDERECO.cep}`;
+            texto += `\n\n*Total (com entrega): R$ ${(VALOR_CARRINHO + VALOR_ENTREGA).toFixed(2).replace('.',',')}*`;
+
+            var itens = '';
+
+            $.each(MEU_CARRINHO, (i, e) =>{
+                itens += `*${e.qntd}x* ${e.name} ........ R$: ${e.price.toFixed(2).replace('.',',')} \n`;
+
+                if((i + 1) == MEU_CARRINHO.length) {
+                    texto = texto.replace(/\${itens}/, itens);
+
+                    let encode = encodeURI(texto);
+                    let URL = `https://wa.me/${CELULAR_EMPRESA}?text=${encode}`;
+
+                    $("#btnEtapaResumo").attr('href', URL);
+                } 
+            })
+        } 
     },
 
     mensagem: (texto,cor = "red", tempo = 3500) => {
